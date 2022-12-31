e38d8269f1f7f2085d8fd19fd246180b27ef817a
Daniel Schmidt <daniel@neonfoto.de>
2022-12-31 16:06:56 +0100

    List all possible tiles.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,57 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <array>
 
-struct Plate
+namespace
 {
-    int x;
-    int y;
-    int z;
-};
+    struct Tile
+    {
+        int x;
+        int y;
+        int z;
+    };
 
 
-std::ostream & operator<<(std::ostream & strm, Plate const & p)
-{
-    strm << p.x << ", " << p.y << ", " << p.z;
-    return strm;
+    std::ostream & operator<<(std::ostream & strm, Tile const & t)
+    {
+        strm << t.x << ", " << t.y << ", " << t.z;
+        return strm;
+    }
+
+
+    std::array<Tile, 27> allPossibleTiles{ 
+        Tile{1, 2, 3}, 
+        {1, 2, 4},
+        {1, 2, 8},
+        {1, 6, 3}, 
+        {1, 6, 4},
+        {1, 6, 8},
+        {1, 7, 3}, 
+        {1, 7, 4},
+        {1, 7, 8},
+        {5, 2, 3}, 
+        {5, 2, 4},
+        {5, 2, 8},
+        {5, 6, 3}, 
+        {5, 6, 4},
+        {5, 6, 8},
+        {5, 7, 3}, 
+        {5, 7, 4},
+        {5, 7, 8},
+        {9, 2, 3}, 
+        {9, 2, 4},
+        {9, 2, 8},
+        {9, 6, 3}, 
+        {9, 6, 4},
+        {9, 6, 8},
+        {9, 7, 3}, 
+        {9, 7, 4},
+        {9, 7, 8}};
 }
 
 
 int main()
 {
-    Plate const p1{9, 7, 3};    
-    std::cout << "Hello Daniel. Du hast " << p1 << " gezogen!\n";
+    Tile const t = allPossibleTiles[23];    
+    std::cout << "Hello Daniel. Du hast " << t << " gezogen!\n";
 }
