0425ca1b4e6dfeb80142d7762a4be6b98d3b1093
Daniel Schmidt <daniel@neonfoto.de>
2023-02-18 19:18:56 +0100

    Implement two easy tests for the Board.

--- test/BoardTests.cpp
+++ test/BoardTests.cpp
@@ -2,6 +2,24 @@
 
 #include <gtest/gtest.h>
 
-TEST(BoardTests, EmptyBoard){
-    FAIL();
+namespace OptiTake
+{
+
+TEST(BoardTests, GetScore_WhenBoardIsEmpty_ScoreIsZero)
+{
+    Board under_test{};
+    auto const result = under_test.GetScore();
+    EXPECT_EQ(result, 0);
+}
+
+TEST(BoardTests, GetScore_WhenBoardHasSingleTile_ScoreIsZero)
+{
+    Board under_test{};
+    bool success = under_test.SetTileToPosition({1, 2, 3}, {1, 1});
+    ASSERT_TRUE(success);
+
+    auto const result = under_test.GetScore();
+    EXPECT_EQ(result, 0);
+}
+
 }
